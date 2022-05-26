edf8535650a364e895242b6a0068025ba38db7bd
tamma-byte <93988570+tamma-byte@users.noreply.github.com>
2022-05-26 16:53:32 +1000

    Add files via upload
    Added main "Program.cs" then the classes in "Movie.cs" for the purposes of practicing the Getter and Setter from Codecademy

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesPractice_GettersSetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GETTERS AND SETTERS help make classes more SECURE
+            Movie avengers = new Movie("Avengers", "Joss Whedon", "PG-13");
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+
+            //EXERCISE: Ratings can only be G, PG, PG-13, R, NR
+            //Because Ratings is Private
+
+            Console.WriteLine(shrek.Rating);
+
+            Console.ReadLine();
+        }
+    }
+}
